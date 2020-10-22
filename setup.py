71a257882ff9a18909b2956ad0b4f2b73505bdc8
Jorge Rodríguez-Araújo <jorge@abraiasoftware.com>
2020-10-22 10:47:16 +0200

    Add setup configuration

--- setup.py
+++ setup.py
@@ -1,5 +1,29 @@
-from setuptools import setup, find_packages
+import pathlib
+import pkg_resources
 
-setup(name='multiple',
-      version='0.1',
-      packages=find_packages())
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [str(requirement) for requirement
+        in pkg_resources.parse_requirements(requirements_txt)]
+
+setup(
+    name='multiple',
+    version='0.1',
+    description='Abraia Multiple SDK',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/abraia/abraia-multiple',
+    author='Jorge Rodriguez Araujo',
+    author_email='jorge@abraiasoftware.com',
+    license='MIT',
+    zip_safe=False,
+    packages=['multiple'],
+    tests_require=['pytest'],
+    setup_requires=['setuptools>=38.6.0', 'pytest-runner'],
+    package_data={'multiple': ['multiple/cie-cmf_1nm.txt']},
+    install_requires=install_requires
+)
